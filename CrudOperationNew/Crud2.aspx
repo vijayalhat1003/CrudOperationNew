74ae95596836199bc3f24e28f577e57a883f7137
Vijay <vijayalhat1003@gmail.com>
2024-02-24 17:39:51 +0530

    Add project files.

---
+++ CrudOperationNew/Crud2.aspx
@@ -0,0 +1,65 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Crud2.aspx.cs" Inherits="CrudOperationNew.Crud2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 72%;
+            height: 211px;
+            margin-left: 192px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <table class="auto-style1">
+            <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>Employee ID</td>
+                <td>
+                    <asp:TextBox ID="txtId" runat="server"></asp:TextBox>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>Emaployee Name</td>
+                <td>
+                    <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>Employee Salary</td>
+                <td>
+                    <asp:TextBox ID="txtSal" runat="server"></asp:TextBox>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td>
+                    <asp:Button ID="btnSave" runat="server" Text="SAVE" OnClick="btnSave_Click" />
+                    <asp:Button ID="btnFind" runat="server" Text="FIND" OnClick="btnFind_Click" />
+                    <asp:Button ID="btnUpdate" runat="server" Text="UPDATE" OnClick="btnUpdate_Click" />
+                    <asp:Button ID="btnDel" runat="server" Text="DELETE" OnClick="btnDel_Click" />
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
---
+++ CrudOperationNew/Crud2.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CrudOperationNew
+{
+    public partial class Crud2 : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-4GKIR43;Initial Catalog=vijay;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            String query = "insert into  Employee2 values('"+txtId.Text+"','"+txtName.Text+"' , '"+txtSal.Text+"')";
+            SqlCommand cmd = new SqlCommand(query,con);
+            con.Open();
+            cmd.ExecuteNonQuery();  
+            con.Close();
+            Response.Write("<script>alert('Data Saved')</script>");
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            String query = "update employee2 set EmpSalary='"+txtSal.Text+"' where EmpId='"+txtId.Text+"'";
+            SqlCommand cmd = new SqlCommand(query, con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Response.Write("<script>alert('Data Updated')</script>");
+        }
+
+        protected void btnDel_Click(object sender, EventArgs e)
+        {
+            String query = "delete from employee2 where EmpId='"+txtId.Text+"'";
+            SqlCommand cmd = new SqlCommand(query, con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Response.Write("<script>alert('Data Deleted')</script>");
+        }
+
+        protected void btnFind_Click(object sender, EventArgs e)
+        {
+            String query = "select * from employee2";
+            SqlCommand cmd = new SqlCommand(query, con);
+            SqlDataReader dr;
+            con.Open();
+            dr=cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                txtName.Text = dr["EmpName"].ToString();
+                txtSal.Text = dr["EmpSalary"].ToString();
+            
+            }
+            dr.Close();
+            con.Close();
+           
+        }
+    }
+}
